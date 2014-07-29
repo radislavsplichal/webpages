490499c743fb27416d753492c1b462f71fe94150
radislavsplichal <radislavsplichal@gmail.com>
2014-07-29 19:45:16 +0200

    remove blabla

--- newuserf.php
+++ newuserf.php
@@ -92,14 +92,3 @@ if (isset($_SESSION['badcaptcha'])){
             </section>
         </div>
     </body>
-
-
-
-
-    <?php
-    /*
-     * To change this license header, choose License Headers in Project Properties.
-     * To change this template file, choose Tools | Templates
-     * and open the template in the editor.
-     */
-    ?>
